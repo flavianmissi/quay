622a6aedf8d5d16b6f890c39bcdd0cc2ffb71a4e
Flavian Missi <fmissi@redhat.com>
2022-01-12 18:33:27 +0100

    proxy: introduce method to get manifest from upstream

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -3,35 +3,67 @@ The proxy module provides the means to proxy images from other registry instance
 Registries following the distribution spec are supported.
 """
 from __future__ import annotations
+from typing import Any
+from urllib.parse import urlparse
 import re
 
 import requests
 
 
+DEFAULT_SCHEME = "https"
 WWW_AUTHENTICATE_REGEX = re.compile(r'(\w+)[=] ?"?([^",]+)"?')
 
 
-class Proxy:
 
+def parse_www_auth(value: str) -> dict[str, str]:
+    """
+    Parses WWW-Authenticate parameters and returns a dict of key=val.
+    See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/WWW-Authenticate
+    for details.
+    """
+    matches = WWW_AUTHENTICATE_REGEX.findall(value)
+    return dict(matches)
+
+
+class Proxy:
     def __init__(self, registry: str, repository: str, auth: tuple[str, str] | None = None):
-        self.url = "https://" + registry + "/v2/"
+        url = registry.rstrip("/")
+        if not registry.startswith("http"):
+            url = f"{DEFAULT_SCHEME}://{registry}"
+        self.base_url = url
         self.session = requests.Session()
         self._repo = repository
-        self._authorize(self._get_auth_url(), auth)
+        self._authorize(auth)
 
-    def _get_auth_url(self) -> str:
-        resp = self.session.get(self.url)
-        www_auth = dict(
-            WWW_AUTHENTICATE_REGEX.findall(resp.headers.get("www-authenticate"))
+    def get_manifest(self, image_ref: str, media_type: str | None = None) -> dict[str, Any]:
+        headers = {}
+        if media_type is not None:
+            # TODO: check specification for a possible default media type
+            headers["Accept"] = media_type
+
+        url = f"{self.base_url}/v2/{self._repo}/manifests/{image_ref}"
+        resp = self.session.get(
+            url,
+            headers=headers,
         )
-        service = www_auth["service"]
+        if not resp.ok:
+            raise Exception(resp.json())
+        return resp.json()
+
+    def _authorize(self, auth: tuple[str, str] | None = None):
+        resp = self.session.get(f"{self.base_url}/v2/")
+
+        # the /v2/ endpoint returns 401 when the client is not authorized.
+        # if we get 200, there's no need to proceed.
+        if resp.status_code == 200:
+            return
+
+        www_auth = parse_www_auth(resp.headers.get("www-authenticate", ""))
+        service = www_auth.get("service")
+        realm = www_auth.get("realm")
+
         scope = f"repository:{self._repo}:pull"
-        auth_url = www_auth["realm"] + f"?service={service}&scope={scope}"
-        print(auth_url)
-
-        return auth_url
-
-    def _authorize(self, auth_url: str, auth: tuple[str, str] | None = None):
+        auth_url =  f"{realm}?service={service}&scope={scope}"
         basic_auth = None
         if auth is not None:
             basic_auth = requests.auth.HTTPBasicAuth(auth[0], auth[1])
--- test/test_proxy.py
+++ test/test_proxy.py
@@ -2,18 +2,40 @@ import unittest
 
 from httmock import urlmatch, response, HTTMock
 
-from proxy import Proxy
+from proxy import Proxy, parse_www_auth
+
+
+class TestWWWAuthParser(unittest.TestCase):
+    realm = "https://auth.docker.io/token"
+    service = "registry.docker.io"
+
+    def test_parse_realm(self):
+        header = f'Bearer realm="{self.realm}",service="{self.service}"'
+        parsed = parse_www_auth(header)
+        self.assertEqual(parsed["realm"], self.realm)
+
+    def test_parse_service(self):
+        header = f'Bearer realm="{self.realm}",service="{self.service}"'
+        parsed = parse_www_auth(header)
+        self.assertEqual(parsed["service"], self.service)
+
+    def test_parse_empty(self):
+        parsed = parse_www_auth("")
+        self.assertEqual(parsed, {})
+
 
 def docker_registry_mock_401(url, request):
     headers = {
-        "www-authenticate": "Bearer realm=\"https://auth.docker.io/token\",service=\"registry.docker.io\""
+        "www-authenticate": 'Bearer realm="https://auth.docker.io/token",service="registry.docker.io"'
     }
     content = {
-        "errors": [{
-            "code": "UNAUTHORIZED",
-            "message": "authentication required",
-            "detail": None,
-        }]
+        "errors": [
+            {
+                "code": "UNAUTHORIZED",
+                "message": "authentication required",
+                "detail": None,
+            }
+        ]
     }
     return response(401, content, headers)
 
@@ -32,37 +54,95 @@ def docker_auth_mock(url, request):
         "token": token,
         "access_token": "access-token",
         "expires_in": 300,
-        "issued_at": "2022-01-11T15:28:36.272470279Z"
+        "issued_at": "2022-01-11T15:28:36.272470279Z",
     }
     return response(200, content)
 
 
-@urlmatch(netloc=r'(.*\.)?docker\.io')
+def docker_registry_manifest(url, request):
+    content = {
+        "schemaVersion": 2,
+        "mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+        "config": {
+            "mediaType": "application/vnd.docker.container.image.v1+json",
+            "size": 10231,
+            "digest": "sha256:07e2ee723e2d9c8c141137bf9de1037fd2494248e13da2805a95ad840f61dd6c",
+        },
+        "layers": [
+            {
+                "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+                "size": 31357624,
+                "digest": "sha256:a2abf6c4d29d43a4bf9fbb769f524d0fb36a2edab49819c1bf3e76f409f953ea",
+            }
+        ],
+    }
+    return response(200, content)
+
+
+def docker_registry_manifest_404(url, request):
+    content = {
+        "errors": [
+            {"code": "MANIFEST_UNKNOWN", "message": "manifest unknown", "detail": "unknown tag=666"}
+        ]
+    }
+    return response(404, content)
+
+
+@urlmatch(netloc=r"(.*\.)?docker\.io")
 def docker_registry_mock(url, request):
     if url.netloc == "registry-1.docker.io":
-        return docker_registry_mock_401(url, request)
+        if url.path == "/v2" or url.path == "/v2/":
+            return docker_registry_mock_401(url, request)
+        elif url.path == "/v2/library/postgres/manifests/14":
+            return docker_registry_manifest(url, request)
+        elif url.path == "/v2/library/postgres/manifests/666":
+            return docker_registry_manifest_404(url, request)
     elif url.netloc == "auth.docker.io":
         return docker_auth_mock(url, request)
-    return response(404, f"ops, this endpoint isn't mocked. requested {url.netloc}")
+
+    msg = {
+        "errors": [
+            {"message": f"Oops, this endpoint isn't mocked. requested {url.netloc}/{url.path}"}
+        ]
+    }
+    return response(404, msg)
 
 
 class TestProxy(unittest.TestCase):
-
     def test_anonymous_auth_sets_session_token(self):
         with HTTMock(docker_registry_mock):
             proxy = Proxy("registry-1.docker.io", "library/postgres")
             self.assertEqual(
-                proxy.session.headers.get("Authorization"),
-                f"Bearer {ANONYMOUS_TOKEN}"
+                proxy.session.headers.get("Authorization"), f"Bearer {ANONYMOUS_TOKEN}"
             )
 
     def test_auth_with_user_creds_set_session_token(self):
         with HTTMock(docker_registry_mock):
-            proxy = Proxy(
-                "registry-1.docker.io",
-                "library/postgres",
-                auth=("user", "pass"))
-            self.assertEqual(
-                proxy.session.headers.get("Authorization"),
-                f"Bearer {USER_TOKEN}"
+            proxy = Proxy("registry-1.docker.io", "library/postgres", auth=("user", "pass"))
+            self.assertEqual(proxy.session.headers.get("Authorization"), f"Bearer {USER_TOKEN}")
+
+    def test_get_manifest(self):
+        with HTTMock(docker_registry_mock):
+            proxy = Proxy("registry-1.docker.io", "library/postgres")
+            manifest = proxy.get_manifest(
+                image_ref="14", media_type="application/vnd.docker.distribution.manifest.v2+json"
             )
+            self.assertEqual(
+                list(manifest.keys()), ["schemaVersion", "mediaType", "config", "layers"]
+            )
+
+    def test_get_manifest_404(self):
+        with HTTMock(docker_registry_mock):
+            proxy = Proxy("registry-1.docker.io", "library/postgres")
+            with self.assertRaises(Exception) as cm:
+                manifest = proxy.get_manifest(
+                    image_ref="666",
+                    media_type="application/vnd.docker.distribution.manifest.v2+json",
+                )
+        unknown_manifest = {
+            "code": "MANIFEST_UNKNOWN",
+            "message": "manifest unknown",
+            "detail": "unknown tag=666",
+        }
+        error = cm.exception.args[0].get("errors", [{}])[0]
+        self.assertEqual(unknown_manifest, error)
